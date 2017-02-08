7590a1981a660a386de58cb1c42169ff3dbe9abc
LittleCoder <i7meavnktqegm1b@qq.com>
2017-02-08 17:01:00 +0800

    Fix name conflict in py2

--- itchatmp/views/__init__.py
+++ itchatmp/views/__init__.py
@@ -1,2 +1,2 @@
-from .struct import deconstruct_msg, construct_msg, reply_msg_format
+from .msgconstruct import deconstruct_msg, construct_msg, reply_msg_format
 from .crypto import decrypt_msg, encrypt_msg
